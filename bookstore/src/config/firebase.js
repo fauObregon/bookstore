a5891603a6424c66fc383d7cc5a0c0a88eb0db49
Fausto Obregón <Fausto Obregn>
2018-04-21 16:26:19 -0500

    Primer dia de curso

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyC51aIwQn8Kop58tO0oO6lF_tzQAQ6kuCI",
+    authDomain: "bookstore-b5d0c.firebaseapp.com",
+    databaseURL: "https://bookstore-b5d0c.firebaseio.com",
+    projectId: "bookstore-b5d0c",
+    storageBucket: "bookstore-b5d0c.appspot.com",
+    messagingSenderId: "492392197841"
+};
